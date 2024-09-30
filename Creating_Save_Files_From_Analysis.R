9b3b818ae7fade95ae869cbf205a9405a2880936
majernik14 <majernik.14@osu.edu>
2024-09-30 14:21:04 -0400

    Updates

--- Creating_Save_Files_From_Analysis.R
+++ Creating_Save_Files_From_Analysis.R
@@ -6,18 +6,26 @@ Wrt_Gene_Reads_Path <- "~/Documents/Bradley_Lab/MIDAS_Analysis_Main_Folder/PanSw
 Wrt_Species_id_cor_and_orig <- "~/Documents/Bradley_Lab/MIDAS_Analysis_Main_Folder/PanSweep_Updated_Data_Analysis/Updated_PanSweep_Figure_Code/Species_id_cor_and_orig.rds"
 Egg_Save <- "~/Documents/Bradley_Lab/MIDAS_Analysis_Main_Folder/PanSweep_Updated_Data_Analysis/Updated_PanSweep_Figure_Code/uhgp_90_eggNOG.tsv"
 #Create file for gene reads:
-species_set <- unique(Genes_intr_extr$Species_id)
+species_set_corr <- unique(Genes_intr_extr$Species_id)
+#Read in and merge gene counts:
+Gene_reads <- purrr::map(species_set_corr, ~ {
+  gpath <- file.path(path_to_read_counts, .x, paste0(.x, ".genes_reads.tsv"))
+  if (is_compressed) {
+    gpath <- paste0(gpath, ".lz4")
+    tsv <- read_tsv_arrow(gpath)
+  } else {
+    tsv <- read_tsv(gpath, col_types=cols())
+  }
+  merge_columns_tbl(tsv, md=phylo_md2, fn=merge_fn_counts)
+}) %>% setNames(species_set_corr)
 
-Gene_reads <- map(species_set, ~ read_tsv(paste0(Gene_Reads_Paths,
-                                                 .x,
-                                                 "/",
-                                                 .x,
-                                                 ".genes_reads.tsv"),
-                                          col_types=cols())) %>% setNames(species_set)
-Sig_Gene_Reads <- map(species_set,  ~dplyr::filter(Gene_reads[[.x]], gene_id %in% Genes_intr_extr$Gene_id) %>% pivot_longer(!gene_id, names_to = "run", values_to = "Gene_count") %>% pivot_wider(names_from = "gene_id", values_from = "Gene_count") %>% column_to_rownames("run")) %>% setNames(species_set)
-
+Sig_Gene_reads <- purrr::map(species_set_corr, ~ {
+  dplyr::filter(Gene_reads[[.x]], gene_id %in% Genes_intr_extr$Gene_id) %>%
+    column_to_rownames("gene_id") %>%
+    as.matrix()
+}) %>% setNames(species_set_corr)
 #Save as RDS for later loading:
-write_rds(Sig_Gene_Reads, file = Wrt_Gene_Reads_Path)
+write_rds(Sig_Gene_reads, file = Wrt_Gene_Reads_Path)
 
 #Save Max correlation and originating species information for each gene
 write_rds(Species_Cor_DF, file = Wrt_Species_id_cor_and_orig)
